fd799f2b9f9b934e64c46cfa423808402893507e
Nitin Patel <codeconductor0607@gmail.com>
2025-12-02 23:42:07 +0530

    3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Shop from './Pages/Shop'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Shop />} />
+      </Routes>
     </div>
   )
 }
